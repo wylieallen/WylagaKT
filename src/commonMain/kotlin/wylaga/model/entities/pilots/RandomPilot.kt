0499f039f99557abb17b5dd1f19cf2532fe8c591
wylieallen <wylieallen1@gmail.com>
2019-01-08 18:36:43 -0500

    RandomPilot now constrains itself to the visible area

--- src/commonMain/kotlin/wylaga/model/entities/pilots/RandomPilot.kt
+++ src/commonMain/kotlin/wylaga/model/entities/pilots/RandomPilot.kt
@@ -4,9 +4,25 @@ import wylaga.model.entities.ships.Ship
 import wylaga.util.DirectionVector
 import kotlin.random.Random
 
-class RandomPilot(private val redirectChance: Double, private val fireChance: Double, private val toggleBoostChance: Double) : Pilot {
+class RandomPilot(private val redirectChance: Double, private val fireChance: Double, private val toggleBoostChance: Double,
+                  private val maxX: Double = 1550.0, private val maxY: Double = 400.0) : Pilot {
     override fun update(ship: Ship) {
-        if(Random.nextDouble() <= redirectChance) {
+        if(ship.x <= 0.0 || ship.x >= maxX || ship.y <= 0.0 || ship.y >= maxY) {
+            val dx = when {
+                ship.x <= 0.0 -> 1.0
+                ship.x >= maxX -> -1.0
+                else -> Random.nextDouble() * 2 - 1
+            }
+
+            val dy = when {
+                ship.y <= 0.0 -> 1.0
+                ship.y >= maxY -> -1.0
+                else -> Random.nextDouble() * 2 - 1
+            }
+
+            ship.trajectory = DirectionVector(dx, dy)
+        }
+        else if(Random.nextDouble() <= redirectChance) {
             ship.trajectory = DirectionVector.random()
         }
 
