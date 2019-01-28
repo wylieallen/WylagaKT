bb23b34f6f028686a622e8196fe50614b8ef2010
wylieallen <wylieallen1@gmail.com>
2019-01-28 18:34:10 -0500

    Optimized Starfield by grouping stars into moving layers

--- src/commonMain/kotlin/wylaga/view/backgrounds/Starfield.kt
+++ src/commonMain/kotlin/wylaga/view/backgrounds/Starfield.kt
@@ -10,18 +10,24 @@ import wylaga.view.display.tickables.Tickable
 import wylaga.view.display.tickables.composites.CompositeTickable
 import kotlin.random.Random
 
-class Starfield(width: Double, private val height: Double, starCount: Int) : Displayable, Tickable {
+const val LAYERS = 4
+
+class Starfield(private val width: Double, private val height: Double, starCount: Int) : Displayable, Tickable {
     private val displayRoot = CompositeDisplayable()
     private val tickRoot = CompositeTickable()
 
     init {
         displayRoot.add(SolidRect(width, height, Color.BLACK))
 
-        for(i in 1..starCount) {
-            val star = Star(Random.nextDouble() * width, Random.nextDouble() * height, Random.nextDouble() * 3 + 0.2, 1.0,
-                Color(Random.nextInt(0xFF), Random.nextInt(0xFF), Random.nextInt(0xFF)))
-            displayRoot.add(star)
-            tickRoot.add(star)
+        val starsPerLayer = starCount / LAYERS
+
+        for(i in 1..LAYERS) {
+            val topLayer = StarLayer(0.0, -height, i, starsPerLayer)
+            val bottomLayer = StarLayer(0.0, 0.0, i, starsPerLayer)
+            displayRoot.add(topLayer)
+            displayRoot.add(bottomLayer)
+            tickRoot.add(topLayer)
+            tickRoot.add(bottomLayer)
         }
     }
 
@@ -29,16 +35,31 @@ class Starfield(width: Double, private val height: Double, starCount: Int) : Dis
 
     override fun tick() = tickRoot.tick()
 
-    private inner class Star(x: Double, y: Double, private val dy: Double, size: Double, color: Color) : Displayable, Tickable {
-        private val displayable = TranslatedDisplayable(x, y, SolidRect(size, size, color))
+    private inner class StarLayer(x: Double, y: Double, private val dy: Int, starCount: Int) : Displayable, Tickable {
+        private val displayable: TranslatedDisplayable
+
+        init {
+            val composite = CompositeDisplayable()
+            displayable = TranslatedDisplayable(x, y, composite)
+
+            for(i in 1..starCount) {
+                composite.add(Star(Random.nextDouble(width), Random.nextDouble(height), 1.0 + Random.nextInt(2), Color(Random.nextInt(0xFF), Random.nextInt(0xFF), Random.nextInt(0xFF))))
+            }
+        }
 
         override fun display(p: Painter) = displayable.display(p)
 
         override fun tick() {
             displayable.y += dy
             if(displayable.y >= height) {
-                displayable.y = 0.0
+                displayable.y = -height
             }
         }
+    }
+
+    private class Star(x: Double, y: Double, size: Double, color: Color) : Displayable {
+        private val displayable = TranslatedDisplayable(x, y, SolidRect(size, size, color))
+
+        override fun display(p: Painter) = displayable.display(p)
     }
 }
